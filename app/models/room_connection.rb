b7351f07ff946fa2396a51dd09f85271fe316002
JustinHodge <justinhodge8@gmail.com>
2023-05-31 16:09:03 -0400

    fix room selector for connection

--- app/models/room_connection.rb
+++ app/models/room_connection.rb
@@ -1,4 +1,4 @@
 class RoomConnection < ApplicationRecord
-  belongs_to :room_a_id
-  belongs_to :room_b_id
+  belongs_to :room_a_id, class_name:"Room"
+  belongs_to :room_b_id, class_name:"Room"
 end
